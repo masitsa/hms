19f33d5d0e9dcdad7e026a7f51b09e0ddccf4c55
marttkip <marttkip@gmail.com>
2014-09-08 12:37:27 +0300

    my commit

--- application/modules/nurse/controllers/nurse.php
+++ application/modules/nurse/controllers/nurse.php
@@ -13,14 +13,22 @@ class Nurse extends auth
 	
 	public function index()
 	{
+		echo "no patient id";
 	}
 	
 	public function nurse_queue()
 	{
+
 	}
 	
-	public function patient_card($visit_id)
+	public function patient_card($visit_id=1)
 	{
+		$v_data['visit_id'] = $visit_id;
+		$data['content'] = $this->load->view('patient_card', $v_data, true);
+		
+		$data['title'] = 'Patient Card';
+		$data['sidebar'] = 'nurse_sidebar';
+		$this->load->view('auth/template_sidebar', $data);	
 	}
 }
 ?>
