4a3559098a1a0fb8ebb6529d1e88d2c0ca660140
Alvaro Masitsa <alvaromasitsa104@gmail.com>
2014-12-04 11:31:14 +0300

    Changes

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php date_default_timezone_set('Africa/Nairobi');
+<?php //date_default_timezone_set('Africa/Nairobi');
 /*
  *---------------------------------------------------------------
  * APPLICATION ENVIRONMENT
